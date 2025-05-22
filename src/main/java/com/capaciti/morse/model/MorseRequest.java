7624b2531b923208b0eac312a0906ad8dfabefde
Katlego Rampedi <131256326+Katlego-Ram2@users.noreply.github.com>
2025-05-22 15:51:14 +0200

    docs: Add Javadoc to MorseRequest model class

--- src/main/java/com/capaciti/morse/model/MorseRequest.java
+++ src/main/java/com/capaciti/morse/model/MorseRequest.java
@@ -1,19 +1,45 @@
 package com.capaciti.morse.model;
 
+/**
+ * MorseRequest is a simple model class that represents the input
+ * message to be encoded to or decoded from Morse code.
+ */
 public class MorseRequest {
+
+    /**
+     * The message to encode or decode.
+     */
     private String message;
 
+    /**
+     * Default constructor.
+     */
     public MorseRequest() {
     }
 
+    /**
+     * Constructs a MorseRequest with the specified message.
+     *
+     * @param message The input message (plain text or Morse code).
+     */
     public MorseRequest(String message) {
         this.message = message;
     }
 
+    /**
+     * Returns the message to be processed.
+     *
+     * @return The message string.
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Sets the message to be processed.
+     *
+     * @param message The message string to set.
+     */
     public void setMessage(String message) {
         this.message = message;
     }
